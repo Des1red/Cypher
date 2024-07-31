dd601200addf57fa1595431b77651aeca821204f
Terminal <60133152+Des1red@users.noreply.github.com>
2024-07-31 20:30:51 +0300

    Update setup-windows.ps1

--- docker/scripts/setup-windows.ps1
+++ docker/scripts/setup-windows.ps1
@@ -7,7 +7,7 @@ if (-not (Get-Command docker -ErrorAction SilentlyContinue)) {
 # Determine the PowerShell profile path
 $configFile = $PROFILE
 
-Write-Host "Configuration file set to $configFile"
+Write-Host "Configuration file set to ${configFile}"
 
 # Move to the root directory of the project before building the Docker image
 cd (Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) "..")
@@ -22,10 +22,10 @@ $aliasCommand = "function cypher { docker run -it --rm --network host --cap-add 
 if (-not (Select-String -Path $configFile -Pattern "cypher")) {
     Add-Content -Path $configFile -Value $aliasCommand
     . $configFile
-    Write-Host "Alias added to $configFile:"
+    Write-Host "Alias added to ${configFile}:"
     Get-Content $configFile | Select-String -Pattern "cypher"
 } else {
-    Write-Host "Alias cypher already exists in $configFile"
+    Write-Host "Alias cypher already exists in ${configFile}"
 }
 
 Write-Host "Docker image built and alias 'cypher' set up. You can now run the container using the 'cypher' command."
